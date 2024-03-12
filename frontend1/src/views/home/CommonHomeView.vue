93a5cf7f7e0834c04feadb14f21cd21be4699c9e
syamkarni <syamkarni@gmail.com>
2024-03-12 11:53:33 +0000

    added navbar

--- frontend1/src/views/home/CommonHomeView.vue
+++ frontend1/src/views/home/CommonHomeView.vue
@@ -4,13 +4,37 @@
         <title>Bootstrap Navbar</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       </head>
-      
-              <button @click="logout">Logout</button>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                  data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+                  aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  <a class="nav-link" href="#">Features</a>
+                  <a class="nav-link" href="#">Pricing</a>
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </div>
+
+              <form class="d-flex ms-auto">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+
+              <button class="btn btn-outline-danger ms-2" type="button" @click="logout">Sign Out</button>
+          </div>
+      </div>
+  </nav>
+  <div class="Navbar_padding">
 
       <UserHome v-if="userRole == 'user'" />
       <AdminHome v-if="userRole == 'admin'" />
       <LibrarianHome v-if="userRole == 'librarian'" />
-      
+      </div>
     </div>
   </template>
   
@@ -49,5 +73,8 @@
   
   <style scoped>
   /* Your styles here */
+  .Navbar_padding {
+    padding-top: 56px; /* Adjust according to your navbar's height */
+  }
   </style>
   

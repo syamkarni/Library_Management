f90d9361d4f06452bf3ed8048ae9ef7104178dc7
syamkarni <syamkarni@gmail.com>
2024-03-18 12:52:34 +0000

    incomplete frontend for library view for the sake of commiting to the main branch

--- frontend1/src/views/addons/AddSection.vue
+++ frontend1/src/views/addons/AddSection.vue
@@ -10,39 +10,44 @@
         <label for="sectionDescription" class="form-label">Description</label>
         <textarea class="form-control" id="sectionDescription" v-model="section.description"></textarea>
       </div>
-      <div class="mb-3">
-        <label for="creationDate" class="form-label">Date Created</label>
-        <input type="date" class="form-control" id="creationDate" v-model="section.dateCreated" required>
-      </div>
+
       <button type="submit" class="btn btn-primary">Add Section</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'; // Import Axios
+
 export default {
   name: 'AddSection',
   data() {
     return {
       section: {
         name: '',
-        description: '',
-        dateCreated: ''
+        description: ''
       }
     };
   },
   methods: {
     submitForm() {
-      // Here you can handle the form submission.
-      // For instance, sending the data to your backend server.
-      console.log('Section data:', this.section);
-      // Clear the form
-      this.section = { name: '', description: '', dateCreated: '' };
-      alert("Section added successfully");
+      axios.post('http://127.0.0.1:8081/api/sections', this.section)
+        .then(response => {
+          console.log('Response:', response);
+          // Handle the response from the server here
+          alert("Section added successfully");
+          // Clear the form
+          this.section = { name: '', description: ''};
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          alert("Failed to add section");
+        });
     }
   }
 };
 </script>
+
 
 <style scoped>
 .add-section-container {
